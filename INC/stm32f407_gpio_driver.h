95e2ca87f584bac0c4471d1d0cecdeaaebab8eb4
LAPTOP-A76MUA6E\Linh <lequanglinh1412@gmail.com>
2021-12-19 15:07:36 +0700

    no message

---
+++ INC/stm32f407_gpio_driver.h
@@ -0,0 +1,67 @@
+#define __IO    volatile
+
+//#ifdef INC_STM32F407XX_GPIO_DRIVER_H_
+#define INC_STM32F407XX_GPIO_DRIVER_H_
+#include "stm32f407xx.h"
+
+
+/*
+*   gpio register definition structure
+*/
+typedef struct
+{
+    __IO uint32_t MODER;
+    __IO uint32_t OTYPER;
+    __IO uint32_t OSPEED;
+    __IO uint32_t PUPDR;
+    __IO uint32_t IDR;
+    __IO uint32_t ODR;
+    __IO uint32_t OSRR;
+    __IO uint32_t LCKR;
+    __IO uint32_t AFR[2];                   /*AFR[0] = AFRLow, AFR[1] = AFRHigh*/
+}GPIO_RegDef_t;
+
+
+typedef struct
+{
+    uint8_t GPIO_PinNumber;
+    uint8_t GPIO_PinMode;
+    uint8_t GPIO_Speed;
+    uint8_t GPIO_PuPdControl;
+    uint8_t GPIOOPType;
+    uint8_t GPIO_PinAltFunMode;
+}GPIO_PinConfig_t;
+
+typedef enum
+{
+    GPIO_PIN_0 = 0,
+    GPIO_PIN_1,
+    GPIO_PIN_2,
+    GPIO_PIN_3,
+    GPIO_PIN_4,
+    GPIO_PIN_5,
+    GPIO_PIN_6,
+    GPIO_PIN_7,
+    GPIO_PIN_8,
+    GPIO_PIN_9,
+    GPIO_PIN_10,
+    GPIO_PIN_11,
+    GPIO_PIN_12,
+    GPIO_PIN_13,
+    GPIO_PIN_14,
+    GPIO_PIN_15
+}GPIO_PIN_NUMBER;
+
+void GPIO_Init(void);
+void GPIO_PeriClockControl();
+void GPIO_TogglePin();
+void GPIO_ReadFromInputPin();
+void GPIO_ReadFromInputPort();
+void GPIO_WriteOutputPort();
+void GPIO_WriteOutputPin();
+
+
+
+
+
+#endif
---
+++ INC/stm32f407_rcc_driver.h
@@ -0,0 +1,52 @@
+
+
+#ifndef INC_STM32F407XX_RCC_DRIVER_H_
+#define INC_STM32F407XX_RCC_DRIVER_H_
+
+#include "stm32f407xx.h"
+
+/*
+*   rcc register definition structure
+*/
+typedef struct
+{
+    __IO uint32_t CR;
+    __IO uint32_t PLLCFGR;
+    __IO uint32_t CFGR;
+    __IO uint32_t CIR;
+    __IO uint32_t AHB1RSTR;
+    __IO uint32_t AHB2RSTR;
+    __IO uint32_t AHBB3RSTR;
+    __IO uint32_t RESERVED1;
+    __IO uint32_t APB1RSTR;
+    __IO uint32_t APB2RSTR;
+    __IO uint32_t RESERVED2;
+    __IO uint32_t RESERVED3;
+    __IO uint32_t AHB1ENR;
+    __IO uint32_t AHB2ENR;
+    __IO uint32_t AHB3ENR;
+    __IO uint32_t RESERVED4;
+    __IO uint32_t APB1ENR;
+    __IO uint32_t APB2ENR;
+    __IO uint32_t RESERVED5;
+    __IO uint32_t RESERVED6;
+    __IO uint32_t AHB1LPENR;
+    __IO uint32_t AHB2LPENR;
+    __IO uint32_t AHB3LPENR;
+    __IO uint32_t RESERVED7;
+    __IO uint32_t APB1LPENR;
+    __IO uint32_t APB2LPENR;
+    __IO uint32_t RESERVED;
+    __IO uint32_t RESERVED;
+    __IO uint32_t BDCR;
+    __IO uint32_t CSR;
+    __IO uint32_t RESERVED;
+    __IO uint32_t RESERVED;
+    __IO uint32_t SSCGR;
+    __IO uint32_t PLLI2SCFGR;
+    __IO uint32_t PLLSAICFGR;
+    __IO uint32_t DCKCFGR;
+}RCC_RegDef_t;
+
+
+#endif /*INC_STM32F407XX_RCC_DRIVER_H_*/
